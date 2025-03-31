7f7f2ee99fc3bb874c89d63a455d7cb258eee0ed
Nghĩa Nguyễn <nghianguyentran92@gmail.com>
2025-03-31 17:10:24 +0700

    fix: orgunit selector refreshed unexpectedly

--- src/ui/Desktop/FacilitiesManagement/FacilitiesHierarchy.jsx
+++ src/ui/Desktop/FacilitiesManagement/FacilitiesHierarchy.jsx
@@ -8,7 +8,7 @@ import {
   DataTableBody,
   DataTableColumnHeader,
   Pagination,
-  OrganisationUnitTree
+  OrganisationUnitTree,
 } from "@dhis2/ui";
 import useMetadataStore from "@/states/metadata";
 import { useShallow } from "zustand/react/shallow";
@@ -16,7 +16,7 @@ import { useTranslation } from "react-i18next";
 import _ from "lodash";
 import { pickTranslation } from "@/utils";
 import useFacilityCheckModuleStore from "@/states/facilityCheckModule";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { data } from "react-router";
 
 const FacilityHierarchy = () => {
@@ -28,14 +28,14 @@ const FacilityHierarchy = () => {
   const { selectedOrgUnit, actions } = useFacilityCheckModuleStore(
     useShallow((state) => ({
       actions: state.actions,
-      selectedOrgUnit: state.selectedOrgUnit
+      selectedOrgUnit: state.selectedOrgUnit,
     }))
   );
   const { selectOrgUnit } = actions;
   const { orgUnits, me } = useMetadataStore(
     useShallow((state) => ({
       orgUnits: state.orgUnits,
-      me: state.me
+      me: state.me,
     }))
   );
   const orgUnitInternalState = JSON.parse(JSON.stringify(orgUnits));
@@ -43,7 +43,13 @@ const FacilityHierarchy = () => {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       if (orgUnitInternalState && searchValue) {
-        setFiterOU(orgUnitInternalState.filter(({ name }) => name.toLowerCase().includes(searchValue.toLocaleLowerCase())).map(({ path }) => path));
+        setFiterOU(
+          orgUnitInternalState
+            .filter(({ name }) =>
+              name.toLowerCase().includes(searchValue.toLocaleLowerCase())
+            )
+            .map(({ path }) => path)
+        );
       }
       if (!searchValue) {
         setFiterOU([]);
@@ -52,22 +58,34 @@ const FacilityHierarchy = () => {
     return () => clearTimeout(timeoutId);
   }, [searchValue]);
 
-  const cutString = (str, start) => (str.includes(start) ? str.substring(str.indexOf(start)) : "");
+  const cutString = (str, start) =>
+    str.includes(start) ? str.substring(str.indexOf(start)) : "";
 
   const foundMeOrgUnits = me.organisationUnits.map((ou) => {
-    const foundOu = orgUnitInternalState.find((orgUnit) => orgUnit.id === ou.id);
+    const foundOu = orgUnitInternalState.find(
+      (orgUnit) => orgUnit.id === ou.id
+    );
     return foundOu;
   });
 
   useEffect(() => {
     orgUnitInternalState.forEach((value) => {
-      const foundRoot = foundMeOrgUnits.find((root) => value.path.includes(root.id));
+      const foundRoot = foundMeOrgUnits.find((root) =>
+        value.path.includes(root.id)
+      );
       if (foundRoot) {
         value.path = cutString(value.path, `/${foundRoot.id}`);
       }
     });
     setOrgUnitData(orgUnitInternalState);
-    setKey(filterOU.join("") + new Date().getTime());
+    if (filterOU.length > 0) {
+      const newKey = filterOU.join("") + new Date().getTime();
+      if (key !== newKey) {
+        setKey(newKey);
+      }
+    } else if (key !== "default") {
+      setKey("default");
+    }
   }, [filterOU]);
 
   const returnFilterWithOuRoots = () => {
@@ -98,12 +116,22 @@ const FacilityHierarchy = () => {
       </div>
       <div className="w-full h-[calc(100%-50px)] overflow-auto">
         <OrganisationUnitTree
-          initiallyExpanded={filterOU && filterOU.length > 0 ? returnFilterWithOuRoots() : selectedOrgUnit ? [selectedOrgUnit.path] : undefined}
-          highlighted={filterOU && filterOU.length > 0 ? returnFilterWithOuRoots() : []}
+          initiallyExpanded={
+            filterOU && filterOU.length > 0
+              ? returnFilterWithOuRoots()
+              : selectedOrgUnit
+              ? [selectedOrgUnit.path]
+              : undefined
+          }
+          highlighted={
+            filterOU && filterOU.length > 0 ? returnFilterWithOuRoots() : []
+          }
           filter={returnFilterWithOuRoots()}
           key={key}
           onChange={(orgUnit) => {
-            const foundOrgUnit = orgUnitInternalState.find((ou) => ou.id === orgUnit.id);
+            const foundOrgUnit = orgUnitInternalState.find(
+              (ou) => ou.id === orgUnit.id
+            );
             selectOrgUnit(foundOrgUnit);
           }}
           selected={selectedOrgUnit ? [selectedOrgUnit.path] : undefined}
