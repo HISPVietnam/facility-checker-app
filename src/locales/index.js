aba100fc88a490193a0fd89ffdc561d7b69e9e66
Anh Dũng Nguyễn <nguyenanhdung191@gmail.com>
2025-06-04 13:30:48 +0700

    feat: add es, fr, lo, pt, vi to default locales

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,4 +1,14 @@
 import en from "./en";
+import es from "./es";
+import fr from "./fr";
+import lo from "./lo";
+import pt from "./pt";
+import vi from "./vi";
 export default {
-  en
+  en,
+  es,
+  fr,
+  lo,
+  pt,
+  vi
 };
--- src/ui/Desktop/Installation/Installation.jsx
+++ src/ui/Desktop/Installation/Installation.jsx
@@ -7,41 +7,26 @@ import Install from "./Install";
 import CustomizedButton from "@/ui/common/Button";
 import { useTranslation } from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBackwardStep,
-  faCaretLeft,
-  faCaretRight,
-  faForwardStep,
-  faRotateRight,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBackwardStep, faCaretLeft, faCaretRight, faForwardStep, faRotateRight } from "@fortawesome/free-solid-svg-icons";
 import useInstallationModuleStore from "@/states/installationModule";
 import { useShallow } from "zustand/react/shallow";
 import { STEPS } from "@/const";
-import {
-  getMe,
-  getOrgUnits,
-  getOrgUnitGroups,
-  getOrgUnitGroupSets,
-  getOrgUnitGeoJson,
-  getUsers,
-  getSchemas,
-} from "@/api/metadata";
+import { getMe, getOrgUnits, getOrgUnitGroups, getOrgUnitGroupSets, getOrgUnitGeoJson, getUsers, getSchemas } from "@/api/metadata";
 import useMetadataStore from "@/states/metadata";
 import LanguageSelectButton from "@/ui/common/LanguageSelectButton";
 
 const Installation = () => {
   const { t } = useTranslation();
 
-  const { status, step, valid, refreshingMetadata, actions } =
-    useInstallationModuleStore(
-      useShallow((state) => ({
-        status: state.status,
-        step: state.step,
-        valid: state.valid,
-        actions: state.actions,
-        refreshingMetadata: state.refreshingMetadata,
-      }))
-    );
+  const { status, step, valid, refreshingMetadata, actions } = useInstallationModuleStore(
+    useShallow((state) => ({
+      status: state.status,
+      step: state.step,
+      valid: state.valid,
+      actions: state.actions,
+      refreshingMetadata: state.refreshingMetadata
+    }))
+  );
   const metadataActions = useMetadataStore((state) => state.actions);
   const { setMetadata } = metadataActions;
   const { setStep, toggleRefreshingMetadata } = actions;
@@ -81,7 +66,7 @@ const Installation = () => {
       <div className="flex mt-2 mb-2 w-[1200px] justify-between">
         <CustomizedButton
           disabled={step === 0}
-          className="w-[60px]"
+          className="w-[120px]"
           onClick={() => {
             setStep(step - 1);
           }}
@@ -91,12 +76,7 @@ const Installation = () => {
           {t("back")}
         </CustomizedButton>
         <div className="flex items-center gap-2">
-          <CustomizedButton
-            disabled={status === "importing"}
-            loading={refreshingMetadata}
-            primary
-            onClick={refreshMetadata}
-          >
+          <CustomizedButton disabled={status === "importing"} loading={refreshingMetadata} primary onClick={refreshMetadata}>
             <FontAwesomeIcon icon={faRotateRight} />
             &nbsp;
             {t("refreshMetadata")}
@@ -105,7 +85,7 @@ const Installation = () => {
         </div>
         <CustomizedButton
           disabled={!valid || step === STEPS.length - 1}
-          className="w-[60px] justify-self-end"
+          className="w-[120px] justify-self-end"
           onClick={() => {
             setStep(step + 1);
           }}
