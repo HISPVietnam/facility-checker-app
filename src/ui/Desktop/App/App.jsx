02e3827c7ff550acda9476e823beebe869bdf912
Anh Dũng Nguyễn <nguyenanhdung191@gmail.com>
2025-07-31 19:13:04 +0700

    fix: move DHIS2 provider to top level in App.jsx

--- src/ui/Desktop/App/App.jsx
+++ src/ui/Desktop/App/App.jsx
@@ -66,62 +66,60 @@ const App = () => {
   }
 
   return (
-    <div className="w-full h-full text-slate-700">
-      {withHeaderBar && (
-        <Provider
-          config={{
-            appName: "Facility Checker App",
-            appVersion: {
-              full: version.version
-            },
-            apiVersion: "",
-            baseUrl: VITE_BASE_URL,
-            systemInfo: {
-              version: systemInfo.version
-            }
-          }}
-        >
-          <HeaderBar />
-        </Provider>
-      )}
-      <div className={`w-full ${appHeightClassName} flex items-center justify-center`}>
-        {!ready && <CircularLoader />}
-        {ready && !firstRun && (
-          <>
-            {sidebar ? <Sidebar /> : <CollapsedSidebar />}
-            <div className={`h-full ${sidebar ? "w-[calc(100%-300px)]" : "w-[calc(100%-60px)]"} bg-slate-100`}>
-              <div className="h-[60px] w-full bg-white border-b-slate-300 border-b flex items-center p-2">
-                {/* <div>
+    <Provider
+      config={{
+        appName: "Facility Checker App",
+        appVersion: {
+          full: version.version
+        },
+        apiVersion: "",
+        baseUrl: VITE_BASE_URL,
+        systemInfo: {
+          version: systemInfo.version
+        }
+      }}
+    >
+      <div className="w-full h-full text-slate-700">
+        {withHeaderBar && <HeaderBar />}
+        <div className={`w-full ${appHeightClassName} flex items-center justify-center`}>
+          {!ready && <CircularLoader />}
+          {ready && !firstRun && (
+            <>
+              {sidebar ? <Sidebar /> : <CollapsedSidebar />}
+              <div className={`h-full ${sidebar ? "w-[calc(100%-300px)]" : "w-[calc(100%-60px)]"} bg-slate-100`}>
+                <div className="h-[60px] w-full bg-white border-b-slate-300 border-b flex items-center p-2">
+                  {/* <div>
                   <Button icon={<FontAwesomeIcon icon={sidebar ? faCaretLeft : faCaretRight} />} onClick={toggleSidebar}>
                     {sidebar ? t("expand") : t("collapse")}
                   </Button>
                 </div>
                 <VerticalDivider /> */}
-                <Routes>
-                  <Route path="/home" element={<HomeToolbar />} />
-                  <Route path="/" element={<HomeToolbar />} />
-                  <Route path="/facility-check" element={<FacilitiesManagementToolbar />} />
-                  <Route path="/approval" element={<ApprovalToolbar />} />
-                  <Route path="/synchronization" element={<SynchronizationToolbar />} />
-                  <Route path="/configuration" element={<ConfigurationToolbar />} />
-                </Routes>
+                  <Routes>
+                    <Route path="/home" element={<HomeToolbar />} />
+                    <Route path="/" element={<HomeToolbar />} />
+                    <Route path="/facility-check" element={<FacilitiesManagementToolbar />} />
+                    <Route path="/approval" element={<ApprovalToolbar />} />
+                    <Route path="/synchronization" element={<SynchronizationToolbar />} />
+                    <Route path="/configuration" element={<ConfigurationToolbar />} />
+                  </Routes>
+                </div>
+                <div className="h-[calc(100%-60px)] w-full flex items-center justify-center">
+                  <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/facility-check" element={<FacilitiesManagement />} />
+                    <Route path="/approval" element={<Approval />} />
+                    <Route path="/synchronization" element={<Synchronization />} />
+                    <Route path="/configuration" element={<Configuration />} />
+                  </Routes>
+                </div>
               </div>
-              <div className="h-[calc(100%-60px)] w-full flex items-center justify-center">
-                <Routes>
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/" element={<Home />} />
-                  <Route path="/facility-check" element={<FacilitiesManagement />} />
-                  <Route path="/approval" element={<Approval />} />
-                  <Route path="/synchronization" element={<Synchronization />} />
-                  <Route path="/configuration" element={<Configuration />} />
-                </Routes>
-              </div>
-            </div>
-          </>
-        )}
-        {ready && firstRun && <Installation />}
+            </>
+          )}
+          {ready && firstRun && <Installation />}
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
