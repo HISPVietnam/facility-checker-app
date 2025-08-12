dfa1b9c52615bea263258e78c9d6f7eab725fc73
Anh Dũng Nguyễn <nguyenanhdung191@gmail.com>
2025-08-12 10:51:03 +0700

    fix: minor fixes for MiniMap

--- src/ui/Desktop/FacilitiesManagement/MiniMap.jsx
+++ src/ui/Desktop/FacilitiesManagement/MiniMap.jsx
@@ -1,11 +1,5 @@
 import { useRef, useEffect } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  useMap,
-  GeoJSON,
-  Marker,
-} from "react-leaflet";
+import { MapContainer, TileLayer, useMap, GeoJSON, Marker } from "react-leaflet";
 
 const BoundaryLayer = ({ data, point }) => {
   const ref = useRef();
@@ -33,14 +27,13 @@ const BoundaryLayer = ({ data, point }) => {
         layer.bindTooltip(feature?.properties?.name, {
           permanent: true,
           direction: "center",
-          className:
-            "text-[12px] font-bold text-white bg-transparent border-0 shadow-none boundary-label",
+          className: "text-[12px] font-bold text-white bg-transparent border-0 shadow-none boundary-label"
         });
         layer.setStyle({
           weight: 3,
           opacity: 1,
           fillOpacity: 0.1,
-          color: "#ea580c",
+          color: "#ea580c"
         });
       }}
     ></GeoJSON>
@@ -49,10 +42,9 @@ const BoundaryLayer = ({ data, point }) => {
 
 const FacilitiesLayer = ({ point }) => {
   const facilityIcon = new L.divIcon({
-    className:
-      "rounded-full bg-cyan-700 w-[14px] h-[14px] border-[3px] border-white",
+    className: "rounded-full bg-cyan-700 w-[14px] h-[14px] border-[3px] border-white",
     iconSize: [18, 18],
-    html: `<div></div>`,
+    html: `<div></div>`
   });
 
   return <Marker position={point} icon={facilityIcon} />;
@@ -65,13 +57,14 @@ const MiniMap = ({ data, point }) => {
       minZoom={6}
       maxZoom={17}
       className="rounded-lg"
+      scrollWheelZoom={false}
     >
       <TileLayer
-        // attribution={`&copy; <a href="http://www.esri.com/">Esri</a> i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community`}
+        attribution={`&copy; <a href="http://www.esri.com/">Esri</a> i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community`}
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
       />
       <TileLayer
-        // attribution={`&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`}
+        attribution={`&copy; <a href="https://carto.com/attributions">CARTO</a>`}
         url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png"
       />
       <BoundaryLayer data={data} point={point} />
